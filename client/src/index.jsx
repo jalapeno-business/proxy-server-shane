4ed84b1ed3f787fa5bd58ed633d952d4ea53c550
Shane Tajima <shanetajima@gmail.com>
2018-09-17 19:54:22 -0700

    Rearrange files

--- client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Zagat from './zagat';
-
-ReactDOM.render(<Zagat />, document.getElementById('zagat'));
--- proxy/app.js
+++ proxy/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../client', 'dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use((req, res) => {
   const restaurantId = req.path.slice(1).split('/')[0];
   if (restaurantId && Number.isInteger(+restaurantId)) {
-    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
   } else {
     res.send('Error').end();
   }
