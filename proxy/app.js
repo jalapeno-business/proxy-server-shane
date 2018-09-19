cdc1510d9855bb557455b3b56533f217d03f16bd
Shane Tajima <shanetajima@gmail.com>
2018-09-18 17:31:10 -0700

    Server path bug fix

--- proxy/app.js
+++ proxy/app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use((req, res) => {
   const restaurantId = req.path.slice(1).split('/')[0];
-  if (restaurantId && Number.isInteger(+restaurantId)) {
+  if (restaurantId === '' || (restaurantId && Number.isInteger(+restaurantId))) {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
   } else {
     res.send('Error').end();
