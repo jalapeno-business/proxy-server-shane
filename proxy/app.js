fa12cfb8bb9663351bbaeb2731a7912b3bc0f30b
Shane Tajima <shanetajima@gmail.com>
2018-09-20 18:56:04 -0700

    Add ebs paths

--- proxy/app.js
+++ proxy/app.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.all('/api/restaurant/reviews/*', (req, res) => res.redirect(`http://reviews-api.us-west-2.elasticbeanstalk.com${req.path}`));
-app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://localhost:1177${req.path}`));
+app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://zagat-info2.us-west-1.elasticbeanstalk.com/${req.path}`));
 app.all('/api/restaurant/suggestions/*', (req, res) => res.redirect(`http://localhost:1170${req.path}`));
-app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://localhost:8888${req.path}`));
+app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://carousel-dev6.us-west-1.elasticbeanstalk.com/${req.path}`));
 app.all('/api/restaurant/recommendations/*', (req, res) => res.redirect(`http://localhost:3004${req.path}`));
 
 app.use((req, res) => {
