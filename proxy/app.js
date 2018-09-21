320587669eb2dd9b77465f1fecb63b195c436af6
Shane Tajima <shanetajima@gmail.com>
2018-09-21 12:12:13 -0700

    Update bundle paths

--- proxy/app.js
+++ proxy/app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.all('/api/restaurant/reviews/*', (req, res) => res.redirect(`http://reviews-api.us-west-2.elasticbeanstalk.com${req.path}`));
-app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://zagat-info2.us-west-1.elasticbeanstalk.com/${req.path}`));
-app.all('/api/restaurant/suggestions/*', (req, res) => res.redirect(`http://zagat-suggestions-dev.us-west-1.elasticbeanstalk.com/${req.path}`));
-app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://carousel-dev6.us-west-1.elasticbeanstalk.com/${req.path}`));
-app.all('/api/restaurant/recommendations/*', (req, res) => res.redirect(`http://localhost:3004${req.path}`));
+app.all('/api/restaurant/info/*', (req, res) => res.redirect(`http://zagat-info2.us-west-1.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/suggestions/*', (req, res) => res.redirect(`http://zagat-suggestions-dev.us-west-1.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/carousel/*', (req, res) => res.redirect(`http://carousel-dev6.us-west-1.elasticbeanstalk.com${req.path}`));
+app.all('/api/restaurant/recommendations/*', (req, res) => res.redirect(`http://rec-public-dev.us-west-1.elasticbeanstalk.com${req.path}`));
 
 app.use((req, res) => {
   console.log(req.path);
